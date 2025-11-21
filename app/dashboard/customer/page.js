094d3130bed04971d45dd5f0e4134f6c4059d927
waleedabdelaziz <waleed.zezo14@gmail.com>
2025-11-21 16:03:11 +0200

    remove messsage

--- app/dashboard/customer/page.js
+++ app/dashboard/customer/page.js
@@ -1,12 +1,10 @@
 import UsersList from '@/components/ui/UsersList';
 import { getUsers } from "@/lib/services/userService";
-import en from "@/messages/en.json";
 
 export default async  function Page() {
   const users = await getUsers(); // runs on server
     return (
       <>
-      <div><h1>{en["login.title"]}</h1></div>
     <main className="flex items-center justify-center md:h-screen">
          <UsersList users={users} />
     </main>
