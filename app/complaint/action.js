f01d0823492695a8f21e68db814e9e8cd65f3d39
waleedabdelaziz <waleed.zezo14@gmail.com>
2025-12-20 13:34:33 +0200

    add complaint and survey

--- app/complaint/action.js
+++ app/complaint/action.js
@@ -1,12 +1,13 @@
 "use server"
+import { addComplaint } from "@/lib/services/complaintService";
 export async function submitComplaint(formData) {
     try {
         const ticketID = formData.get("ticketID");
         const complaint = formData.get("complaint");
         console.log("Submitting complaint...", { ticketID, complaint });
 
-        // Here you would typically call a service to save the complaint
-        // For example: await addComplaint(ticketID, complaint);    
+        const userId="1"; // Replace with actual user ID retrieval logic
+        await addComplaint(userId, ticketID, complaint);   
         return { ok: true, message: `Complaint for Ticket #${ticketID} submitted successfully!` };
     } catch (error) {
         return { ok: false, message: "Failed to submit complaint" };
--- app/survey/action.js
+++ app/survey/action.js
@@ -1,10 +1,16 @@
 "use server"
+
+import { addSurvey } from "@/lib/services/surveyService";
+
 export async function submitSurvey(formData) {
     try {
         const rate = formData.get("rate");
         const satisfaction = formData.get("satisfaction");
-        const comments=formData.get("comments");
-        console.log("Submitting survey...", { rate, satisfaction, comments });
+        const feedback=formData.get("comments");
+        
+        console.log("Submitting survey...", { rate, satisfaction, feedback });
+        const userId="1"; // Replace with actual user ID retrieval logic
+        await addSurvey(rate, satisfaction, feedback, userId);
             return { ok: true, message: `Survey submitted successfully!` };
     } catch (error) {
             return { ok: false, message: "Failed to submit complaint" };
